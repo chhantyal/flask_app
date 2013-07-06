723edd23e99512a467c5850d4b0c158066a043e1
Nar Kumar <neokya@gmail.com>
2013-07-07 02:11:55 +0530

    run server and initialize db

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 import sqlite3
+from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
@@ -13,3 +14,18 @@ PASSWORD = 'default'
 # application
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+# method to connect to db
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+# initialize the db
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+# run the server
+if __name__ == '__main__':
+    app.run()
